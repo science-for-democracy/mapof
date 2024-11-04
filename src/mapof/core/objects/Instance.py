9c17c2430f0b229ba12d6df0875cd8ac34b4ea39
szufa <s.szufa@gmail.com>
2024-11-04 12:43:55 +0100

    update Instance class

--- src/mapof/core/objects/Instance.py
+++ src/mapof/core/objects/Instance.py
@@ -8,14 +8,19 @@ class Instance(ABC):
                  experiment_id: str,
                  instance_id: str,
                  culture_id: str = None,
-                 **kwargs):
+                 params: dict = None,
+                 **_kwargs):
+
+        if params is None:
+            params = {}
 
         self.experiment_id = experiment_id
         self.instance_id = instance_id
         self.culture_id = culture_id
         self.features = {}
         self.printing_params = {}
-        self.params = kwargs
+        self.params = params
+
 
         for key in ['color', 'alpha', 'marker', 'ms']:
             self.printing_params[key] = None
